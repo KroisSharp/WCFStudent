eca31b4655fcdf9fff54bed5cd16ef904d0840e4
kroissharp <mikx0067@edu.easj.dk>
2018-04-19 17:33:05 +0200

    Add project files.

---
+++ StudentClient/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentClient
+{
+    class Program
+    {
+
+        //metode til at få fat i connectionString i Appconfig
+        private static string GetConnectionString()
+        {
+            ConnectionStringSettingsCollection connectionStringSettingsCollection = ConfigurationManager.ConnectionStrings;
+            ConnectionStringSettings connStringSettings = connectionStringSettingsCollection["MikDatabaseAzure"];
+            return connStringSettings.ConnectionString;
+        }
+
+        static void Main(string[] args)
+        {
+            //vi øsnker at lave quary som hvis vi selv bruger den
+            string GetAll = "select * from Student";
+
+            //vores string til databasen
+            string DatabaseLinje = GetConnectionString();
+            //man finder den ved at gå ind på:
+            ///1. går ind på azure
+            /// 2. tryk på din sql
+            /// 3. connection strings og brug ADO.NET
+    
+
+            
+
+            using (SqlConnection DBconnection = new SqlConnection(DatabaseLinje))
+            {
+                DBconnection.Open();
+                using (SqlCommand SelectCommand = new SqlCommand(GetAll, DBconnection))
+                {
+                    using (SqlDataReader reader = SelectCommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            int alder = reader.GetInt32(0);
+                            string navn = reader.GetString(1);
+                            Console.WriteLine($"ID:{alder} Navn: {navn}");
+                        }
+                    }
+                }
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ WcfStudent/IService1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfStudent
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        //[OperationContract]
+        //CompositeType GetDataUsingDataContract(CompositeType composite);
+
+    
+
+        // TODO: Add your service operations here
+
+
+        [OperationContract]
+        void AddStudent(string name, int alder);
+
+
+        [OperationContract]
+        Student FindStudent(string name);
+
+
+        [OperationContract]
+        void RemoveStudent(string name);
+
+
+        [OperationContract]
+        void EditStudent(string name);
+
+
+        [OperationContract]
+        List<Student> GetAlleStudents();
+
+
+
+
+
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    //[DataContract]
+    //public class CompositeType
+    //{
+    //    string stringValue = "Hello ";
+
+    //    bool boolValue = true;
+    //    [DataMember]
+    //    public bool BoolValue
+    //    {
+    //        get { return boolValue; }
+    //        set { boolValue = value; }
+    //    }
+
+    //    [DataMember]
+    //    public string StringValue
+    //    {
+    //        get { return stringValue; }
+    //        set { stringValue = value; }
+    //    }
+    //}
+
+
+    [DataContract]
+    public class Student
+    {
+        [DataMember]
+        public string name { get; set; }
+        [DataMember]
+        public int alder { get; set; }
+
+        [DataMember]
+        public static List<Student> StudentListe { get; set; }
+
+
+        static Student()
+        {
+            StudentListe = new List<Student>();
+        }
+
+
+    }
+}
